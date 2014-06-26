4a60297ba06cf44e965ff081250cf1b971029441
mannygtr <xemax@tiscalinet.it>
2014-06-26 13:51:07 +0000

    old mcdonald

---
+++ script.js
@@ -0,0 +1,7 @@
+var animal = prompt('type the name of an animal');
+var sound = prompt('type the sound of that animal');
+document.write('Old MacDonald had a farm, E-I-E-I-O.<br>'+
+'And on that farm he had a <h2>'+animal+'</h2>, E-I-E-I-O.<br>'+
+'With a <h2>'+sound+'</h2> <h2>'+sound+'</h2> here and a <h2>'+sound+'</h2> <h2>'+sound+'</h2> there<br>'+
+'Here a <h2>'+sound+'</h2>, there a <h2>'+sound+'</h2>, everywhere a <h2>'+sound+'</h2> <h2>'+sound+'</h2><br>'+
+'Old MacDonald had a farm, E-I-E-I-O.');
